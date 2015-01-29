7c7aa2cdb34eac16d879c355ae09ab3712d2cd98
lauranjansen <lauran.jansen@gmail.com>
2015-01-29 14:24:02 -0500

    Create basic framework for editing contacts

--- crm.rb
+++ crm.rb
@@ -4,24 +4,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 $rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bravo.com", "Rockstar"))
+$rolodex.add_contact(Contact.new("Day Z.", "Kutter", "daisy@cutter.com", "Knife guy"))
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bravo.com", "Rockstar"))
+
+$crm_name = "My CRM"
 
 get '/'  do
-	@crm_name = "My CRM"
+	@title = "Home - #{$crm_name}"
 	erb :index
 end
 
 get '/contacts' do
+	@title = "Contacts - #{$crm_name}"
 	@contacts = $rolodex.contacts
   erb :contacts
 end
 
 get '/contacts/new_contact' do
+	@title = "New Contact - #{$crm_name}"
 	erb :new_contact
-end
-
-get '/contacts/:id' do
-	@contact = $rolodex.find_contact(params[:id].to_i)
-	erb :contact
 end
 
 post '/contacts' do
@@ -29,4 +31,24 @@ post '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(new_contact)
 	redirect '/contacts'
+end
+
+get '/contacts/:id' do
+	@contact = $rolodex.find_contact(params[:id].to_i)
+	if @contact
+		@title = "#{@contact.first_name} #{@contact.last_name} - #{$crm_name}"
+    erb :contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get "/contacts/:id/edit" do
+	@contact = $rolodex.find_contact(params[:id].to_i)
+	if @contact
+		@title = "Edit Contact - #{$crm_name}"
+		erb :edit_contact
+	else
+		raise Sinatra::NotFound
+	end
 end
