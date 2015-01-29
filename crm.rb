0c825f9eb05829fe2de27c8ab4a6fb2c582109c7
lauranjansen <lauran.jansen@gmail.com>
2015-01-29 10:15:30 -0500

    Initial commit to start off at the end of day 8.

---
+++ crm.rb
@@ -0,0 +1,32 @@
+require_relative 'contact'
+require_relative 'rolodex'
+require 'sinatra'
+require 'sinatra/reloader'
+
+$rolodex = Rolodex.new
+
+get '/'  do
+	@crm_name = "My CRM"
+	erb :index
+end
+
+get '/contacts' do
+	@contacts = $rolodex.contacts
+  erb :contacts
+end
+
+get '/contacts/new_contact' do
+	erb :new_contact
+end
+
+get '/contacts/:id' do
+	@contact = $rolodex.find_contact(params[:id].to_i)
+	erb :contact
+end
+
+post '/contacts' do
+	# p params: params
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	$rolodex.add_contact(new_contact)
+	redirect '/contacts'
+end
